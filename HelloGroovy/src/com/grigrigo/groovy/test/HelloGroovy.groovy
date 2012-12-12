800219bfa278658c8806e2a474c999ed093377dc
gyujin.jung <junggri@gmail.com>
2012-12-12 15:24:26 +0900

    -테스트용 pacakge 추가 -테스트 파일 이동 -테스트 코드 추가

---
+++ HelloGroovy/src/com/grigrigo/groovy/test/HelloGroovy.groovy
@@ -0,0 +1,26 @@
+package com.grigrigo.groovy.test
+
+//hello groovy
+//class HelloGroovy {
+//	def name
+//	
+//	HelloGroovy(who)	{
+//		name = who[0].toUpperCase() + who[1..-1]
+//	}
+//	
+//	def hello()	{
+//		println "Hello $name!"
+//	}
+//}
+//
+//g = new HelloGroovy('grigri')
+
+//file handling
+myFileDirectory = "D:\\Temp\\"
+myFileName = "groovy_test.txt"
+//println myFileDirectory + myFileName
+myFile = new File(myFileDirectory + myFileName)
+
+printFileLine = { println "file line: " + it }
+
+myFile.eachLine { printFileLine }
